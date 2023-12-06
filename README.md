9be00e87197922ce9838ddd7c0119c5a4fc803b0
sdc9600 <149299980+sdc9600@users.noreply.github.com>
2023-12-06 19:41:50 +1030

    Game working, readme added

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # tic_tac_toe
+
+Plays a game of Tic-Tac-Toe between two human opponents.
+
+Crosses and Noughts take turns placing pieces on a 3x3 grid attempting to get three in a row vertically, horizontally or diagonally.
+
+If the board is filled and neither player is able to get three in a row then the game is  draw.
+
+Board: [1,2,3]
+       [4,5,6]
+       [7.8.9]
+
+On the first players turn they enter a number from 1-9 and a piece will be placed on the board in the place of the number entered, for example: 5.
+
+Board after first turn: [1,2,3]
+                        [4,X,6]
+                        [7,8,9]
+
+The second player then gets a turn and is able to place a piece on any square not already occupied by a placed piece, for example: 6
+
+Board after second turn: [1,2,3]
+                         [4,X,O]
+                         [7,8,9]
+                        
+This pattern continues until one player gets three in a row (a victory) or until the board is filled with neither player getting three in a row (a draw).
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -8,6 +8,28 @@ class TicTacToe
      @turn = 1   
   end
 
+  def game_loop
+    until 1==0 do
+      self.make_move_crosses
+      puts self.print_gameboard
+      if self.check_victory?
+        puts "Crosses have got three in a row and win!"
+        break
+      end
+      if @turn == 10
+        puts "Neither side have got three in a row and the board is filled, the game is a draw!"
+        break
+      end
+      self.make_move_noughts
+      puts self.print_gameboard
+      if self.check_victory?
+        puts "Noughts have got three in a row and win!"
+        break
+      end
+    end
+  end
+
+  private
   def check_victory?
     case
     when @game[0] + @game[1] + @game[2] == "OOO" || @game[0] + @game[1] + @game[2] == "XXX" 
@@ -57,27 +79,6 @@ end
     @gameboard = "--Gameboard--\n| #{@game[0]} | #{@game[1]} | #{@game[2]} |\n|" \
      " #{@game[3]} | #{@game[4]} | #{@game[5]} |\n| #{@game[6]} | #{@game[7]} | #{@game[8]} |\n-------------"
     @turn += 1
-  end
-
-  def game_loop
-    until 1==0 do
-      if @turn == 10
-        puts "Neither side have got three in a row and the board is filled, the game is a draw!"
-        break
-      end
-      self.make_move_crosses
-      puts self.print_gameboard
-      if self.check_victory?
-        puts "Crosses have got three in a row and win!"
-        break
-      end
-      self.make_move_noughts
-      puts self.print_gameboard
-      if self.check_victory?
-        puts "Noughts have got three in a row and win!"
-        break
-      end
-    end
   end
 end
 
