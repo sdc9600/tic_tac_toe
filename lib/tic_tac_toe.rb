f42d20cc75a666f299a01f55b583945d38a5e8e1
sdc9600 <149299980+sdc9600@users.noreply.github.com>
2024-02-18 11:15:29 +1030

    Code re-factored for unit tests

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,7 @@
 # Methods relating to the game of tic-tac-toe
 class TicTacToe
-  
+  attr_accessor :turn, :game
+
   def initialize
     @game = Array.new(9, ' ')
     @gameboard = "--Gameboard--\n| #{@game[0]} | #{@game[1]} | #{@game[2]} |\n|" \
@@ -9,44 +10,41 @@ class TicTacToe
   end
 
   def game_loop
+    location = 100
     until @turn == 10 do
-      player_make_move
+      location = select_valid_location(location)
+      player_make_move(location)
+      @turn += 1
       puts gameboard
       if check_victory?
-        puts "Crosses have got three in a row and win!"
-        puts "Noughts have got three in a row and win!"
-        break
+        return puts "Crosses have got three in a row and win!" if @turn.even?
+        return puts "Noughts have got three in a row and win!" if @turn.odd?
       end
     end
     drawn_game
   end
 
-
-
-
-  private
-
   def drawn_game
-    puts "Neither side have got three in a row and the board is filled, the game is a draw!"
+    p "Neither side have got three in a row and the board is filled, the game is a draw!"
   end
 
   def check_victory?
     case
-    when @game[0, 1, 2].join == "OOO" || @game[0, 1, 2].join == "XXX" 
+    when @game[0..2].join == "OOO" || @game[0..2].join == "XXX" 
       true
-    when @game[3, 4, 5].join == "OOO" || @game[3, 4, 5].join == "XXX" 
+    when @game[3..5].join == "OOO" || @game[3..5].join == "XXX" 
       true
-    when @game[6, 7, 8].join == "OOO" || @game[6, 7, 8].join == "XXX" 
+    when @game[6..8].join == "OOO" || @game[6..8].join == "XXX" 
       true
-    when @game[0, 3, 6].join == "OOO" || @game[0, 3, 6].join == "XXX" 
+    when @game[0] + @game[3] + @game[6] == "OOO" || @game[0] + @game[3] + @game[6] == "XXX"
       true
-    when @game[1, 4, 7].join == "OOO" || @game[1, 4, 7].join == "XXX" 
+    when @game[1] + @game[4] + @game[7] == "OOO" || @game[1] + @game[4] + @game[7] == "XXX"
       true
-    when @game[2, 5, 8].join == "OOO" || @game[2, 5, 8].join == "XXX" 
+    when @game[2] + @game[5] + @game[8] == "OOO" || @game[2] + @game[5] + @game[8] == "XXX"
       true
-    when @game[0, 4, 8].join == "OOO" || @game[0, 4, 8].join == "XXX" 
+    when @game[0] + @game[4] + @game[8] == "OOO" || @game[0] + @game[4] + @game[8] == "XXX"
       true
-    when @game[2, 4, 6].join == "OOO" || @game[2, 4, 6].join == "XXX" 
+    when @game[2] + @game[4] + @game[6] == "OOO" || @game[2] + @game[4] + @game[6] == "XXX" 
       true  
     else
       false
@@ -59,22 +57,31 @@ class TicTacToe
     @gameboard
   end
 
-  def player_make_move
-    location = 10
-    puts 'Crosses to move! Enter an available numeric location on the board (1-9) to place a cross!' if @turn.odd?
-    puts "Noughts to move! Enter an available numeric location on the board (1-9) to place a nought!" if @turn.even?
-    until @game[location-1] == ' ' do
-      location = gets.chomp.to_i
+  def player_make_move(location)
+    if @turn.odd?
+      @game[location - 1] = "X"
+      return @game[location - 1]
     end
-    @turn += 1
-    @game[location - 1] = "O" if @turn.even?
-    @game[location - 1] = "X" if @turn.odd?
+    if @turn.even?
+      @game[location - 1] = "O"
+      return @game[location - 1]
+    end
+  end
+
+  private
+
+  def select_valid_location(location)
+    puts 'Crosses to move! Enter an available numeric location on the board (1-9) to place a cross!' if @turn.odd?
+    puts 'Noughts to move! Enter an available numeric location on the board (1-9) to place a nought!' if @turn.even?
+    until @game[location - 1] == " "
+      location = $stdin.gets.chomp.to_i
+    end
+    location
   end
 end
 
-game = TicTacToe.new
-game.game_loop
-
+#game = TicTacToe.new
+#game.game_loop
 
 
 
